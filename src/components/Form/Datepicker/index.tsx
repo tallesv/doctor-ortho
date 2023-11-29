334fc0d3b0ed2f656a40e93d00146b9a12a85344
talles <talles17.a@hotmail.com>
2023-11-29 17:36:35 -0300

    feat: add Datepciker component

--- src/components/Form/Datepicker/index.tsx
+++ src/components/Form/Datepicker/index.tsx
@@ -1,59 +1,66 @@
-import { ForwardedRef, InputHTMLAttributes, forwardRef } from 'react';
+import { ForwardedRef, forwardRef } from 'react';
+import DatePicker, { ReactDatePicker } from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import { getInputBorderStyle } from '../../../utils/inputBorderStyle';
 import bindClassNames from '../../../utils/bindClassNames';
+import { Control, Controller } from 'react-hook-form';
 
-interface DatepickerProps extends InputHTMLAttributes<HTMLInputElement> {
+interface DatepickerProps {
   label?: string;
+  name?: string;
+  className?: string;
+  required?: boolean;
+  control: Control<any>;
   error?: boolean;
   errorMessage?: string;
 }
 
-const Datepicker = forwardRef(
+const DatepickerComponent = forwardRef(
   (
     {
       label,
+      name,
       error,
-      type,
       errorMessage,
       className,
       required,
+      control,
       ...rest
     }: DatepickerProps,
-    ref: ForwardedRef<HTMLInputElement>,
+    ref: ForwardedRef<ReactDatePicker>,
   ) => {
     const inputBorderStyle = getInputBorderStyle(error);
-
     return (
       <div className="w-full">
         {label && (
-          <label
-            htmlFor={rest.id}
-            className="block mb-2 ml-1 text-sm font-medium text-gray-900"
-          >
+          <label className="block mb-2 ml-1 text-sm font-medium text-gray-900">
             {required && <span className="text-red-500 mr-1">*</span>}
             {label}
           </label>
         )}
-        {/* <FlowbiteDatepicker
-          language="pt-BR"
-          labelTodayButton="Hoje"
-          labelClearButton="Apagar"
-          theme={DatepickerStyle}
-          onChange={e => console.log(e)}
-          {...rest}
-        /> */}
-
-        <input
-          id={rest.id}
-          ref={ref}
-          type="date"
-          className={bindClassNames(
-            `shadow-sm bg-gray-50 border text-gray-900 text-sm rounded-lg w-full block p-2.5`,
-            className || '',
-            inputBorderStyle,
-          )}
-          {...rest}
-        />
+        <div className="w-full">
+          <Controller
+            control={control}
+            name={name || ''}
+            render={({ field }) => {
+              return (
+                <DatePicker
+                  dateFormat="dd/MM/yyyy"
+                  ref={ref}
+                  {...rest}
+                  onChange={date => field.onChange(date, name)}
+                  selected={field.value}
+                  wrapperClassName="w-full"
+                  className={bindClassNames(
+                    `shadow-sm bg-gray-50 border text-gray-900 text-sm rounded-lg w-full block p-2.5`,
+                    className || '',
+                    inputBorderStyle,
+                  )}
+                />
+              );
+            }}
+          />
+        </div>
         {errorMessage && (
           <span className="mt-2 text-sm text-red-600">{errorMessage}</span>
         )}
@@ -62,4 +69,4 @@ const Datepicker = forwardRef(
   },
 );
 
-export default Datepicker;
+export default DatepickerComponent;
