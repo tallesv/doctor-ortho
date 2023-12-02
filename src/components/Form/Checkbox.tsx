0fb8b01576a6cd11532889124d3978e3144e939b
talles <talles17.a@hotmail.com>
2023-12-02 15:51:24 -0300

    feat: add Chackbox component

---
+++ src/components/Form/Checkbox.tsx
@@ -0,0 +1,41 @@
+import { ForwardedRef, forwardRef } from 'react';
+import bindClassNames from '../../utils/bindClassNames';
+import { getInputBorderStyle } from '../../utils/inputBorderStyle';
+import {
+  Checkbox as FlowbiteCheckbox,
+  CheckboxProps as FlowbiteCheckboxProps,
+  Label,
+} from 'flowbite-react';
+
+interface CheckboxProps extends FlowbiteCheckboxProps {
+  label: string;
+  error?: boolean;
+}
+
+const Checkbox = forwardRef(
+  (
+    { label, className, error, ...rest }: CheckboxProps,
+    ref: ForwardedRef<HTMLInputElement>,
+  ) => {
+    const inputBorderStyle = getInputBorderStyle(error);
+
+    return (
+      <div className="flex items-center gap-2 ml-1">
+        <FlowbiteCheckbox
+          ref={ref}
+          {...rest}
+          className={bindClassNames(
+            `text-sky-500`,
+            className || '',
+            inputBorderStyle,
+          )}
+        />
+        <Label className="ml-1 text-sm font-medium text-gray-900">
+          {label}
+        </Label>
+      </div>
+    );
+  },
+);
+
+export default Checkbox;
