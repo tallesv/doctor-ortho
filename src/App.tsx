53930588e45ecd1cda6e9a72abf497d9abb78ea0
Talles <talles17.a@hotmail.com>
2024-01-24 22:37:59 -0300

    chore: add react-query lib

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { Router } from './routes';
 import { AuthProvider } from './hooks/auth';
 import { parseCookies } from 'nookies';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
   const cookies = parseCookies();
   const token = cookies['doctor-ortho.token'];
 
+  const queryClient = new QueryClient();
+
   return (
     <BrowserRouter>
       <AuthProvider token={token}>
-        <Router />
+        <QueryClientProvider client={queryClient}>
+          <Router />
+        </QueryClientProvider>
       </AuthProvider>
     </BrowserRouter>
   );
