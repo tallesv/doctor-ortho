8d128b1c157b642d94498f30685a3983bbafcbcf
talles <talles17.a@hotmail.com>
2024-02-01 16:32:29 -0300

    feat: add users table page

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,39 +1,80 @@
+import { useQuery } from '@tanstack/react-query';
 import { Table } from 'flowbite-react';
+import { api } from '../../client/api';
+import { useState } from 'react';
+import Pagination from '../../components/Pagination';
+import { useSearchParams } from 'react-router-dom';
+import { LoadingLayout } from '../../layout/LoadingLayout';
 
-const mockedUsers = [
-  {
-    id: '1',
-    name: 'John Doe',
-    email: 'johndoe@email.com',
-    type: 'Admin',
-    created_at: new Date(),
-  },
-  {
-    id: '2',
-    name: 'Michael Jackson',
-    email: 'michaeljackson@email.com',
-    type: 'Common',
-    created_at: new Date(),
-  },
-  {
-    id: '3',
-    name: 'Lebron James',
-    email: 'lebronjames@email.com',
-    type: 'Common',
-    created_at: new Date(),
-  },
-];
+type UserProps = {
+  id: string;
+  name: string;
+  email: string;
+  specialty: string;
+  phone_number: string;
+  type: string;
+  created_at: string;
+};
 
 export function Users() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const contentPerPage = 10;
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['users'],
+    queryFn: () => api.get('/users'),
+  });
+
+  if (isLoading) {
+    return <LoadingLayout />;
+  }
+
+  const users: UserProps[] = data?.data;
+
+  //const specialitySelected = searchParams.get('speciality') || 'Ortodontia';
+  const termSearched = searchParams.get('search') || '';
+  const filteredData = users.filter(
+    (user: UserProps) =>
+      user.name.toLowerCase().includes(termSearched.toLowerCase()) ||
+      user.email.toLowerCase().includes(termSearched.toLowerCase()),
+  );
+
+  function handleSearchContent(data: string) {
+    setCurrentPage(1);
+    setSearchParams(state => {
+      if (data === '') {
+        state.delete('search');
+      } else {
+        state.set('search', data);
+      }
+      return state;
+    });
+  }
+
+  // function handleSelectSpeciality(specialitySelected: string) {
+  //   setCurrentPage(1);
+  //   setSearchParams(state => {
+  //     if (specialitySelected === '') {
+  //       state.delete('search');
+  //     } else {
+  //       state.set('speciality', specialitySelected);
+  //     }
+  //     return state;
+  //   });
+  // }
+
   return (
-    <>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <div className="flex items-center justify-between pb-4">
-          <div>
+    <div className="p-2">
+      <div className="flex flex-col sm:flex-row gap-2 sm:items-center justify-between pb-4">
+        {/* <Dropdown
+          label={specialitySelected}
+          renderTrigger={() => (
             <button
               id="dropdownRadioButton"
               data-dropdown-toggle="dropdownRadio"
-              className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+              className="sm:w-52 w-full inline-flex justify-center items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
               type="button"
             >
               <svg
@@ -45,7 +86,7 @@ export function Users() {
               >
                 <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z" />
               </svg>
-              Last 30 days
+              {specialitySelected}
               <svg
                 className="w-2.5 h-2.5 ml-2.5"
                 aria-hidden="true"
@@ -55,177 +96,118 @@ export function Users() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m1 1 4 4 4-4"
                 />
               </svg>
             </button>
-            {/* <!-- Dropdown menu --> */}
-            <div
-              id="dropdownRadio"
-              className="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-              data-popper-reference-hidden=""
-              data-popper-escaped=""
-              data-popper-placement="top"
-              //style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate3d(522.5px, 3847.5px, 0px);"
+          )}
+        >
+          {specialitiesType.map(speciality => (
+            <Dropdown.Item key={speciality}>
+              <div
+                className="flex items-center rounded hover:bg-gray-100 dark:hover:bg-gray-600 cursor-pointer"
+                onClick={() => handleSelectSpeciality(speciality)}
+              >
+                <input
+                  id="filter-radio-example-1"
+                  type="radio"
+                  value={speciality}
+                  defaultChecked={specialitySelected === speciality}
+                  name="filter-radio"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  htmlFor="filter-radio-example-1"
+                  className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+                >
+                  {speciality}
+                </label>
+              </div>
+            </Dropdown.Item>
+          ))}
+        </Dropdown> */}
+        <label htmlFor="table-search" className="sr-only">
+          Search
+        </label>
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <ul
-                className="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
-                aria-labelledby="dropdownRadioButton"
-              >
-                <li>
-                  <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                    <input
-                      id="filter-radio-example-1"
-                      type="radio"
-                      value=""
-                      name="filter-radio"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label
-                      htmlFor="filter-radio-example-1"
-                      className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
-                    >
-                      Last day
-                    </label>
-                  </div>
-                </li>
-                <li>
-                  <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                    <input
-                      checked={false}
-                      id="filter-radio-example-2"
-                      type="radio"
-                      value=""
-                      name="filter-radio"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label
-                      htmlFor="filter-radio-example-2"
-                      className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
-                    >
-                      Last 7 days
-                    </label>
-                  </div>
-                </li>
-                <li>
-                  <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                    <input
-                      id="filter-radio-example-3"
-                      type="radio"
-                      value=""
-                      name="filter-radio"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label
-                      htmlFor="filter-radio-example-3"
-                      className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
-                    >
-                      Last 30 days
-                    </label>
-                  </div>
-                </li>
-                <li>
-                  <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                    <input
-                      id="filter-radio-example-4"
-                      type="radio"
-                      value=""
-                      name="filter-radio"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label
-                      htmlFor="filter-radio-example-4"
-                      className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
-                    >
-                      Last month
-                    </label>
-                  </div>
-                </li>
-                <li>
-                  <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                    <input
-                      id="filter-radio-example-5"
-                      type="radio"
-                      value=""
-                      name="filter-radio"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label
-                      htmlFor="filter-radio-example-5"
-                      className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
-                    >
-                      Last year
-                    </label>
-                  </div>
-                </li>
-              </ul>
-            </div>
+              <path
+                fillRule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
           </div>
-          <label htmlFor="table-search" className="sr-only">
-            Search
-          </label>
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </div>
-            <input
-              type="text"
-              id="table-search"
-              className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search for items"
-            />
-          </div>
+          <input
+            type="text"
+            id="table-search"
+            className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Procure pelo Nome ou Email"
+            onChange={e => handleSearchContent(e.target.value)}
+          />
         </div>
-
+      </div>
+      <div className="overflow-x-auto sm:rounded-lg">
         <Table hoverable>
           <Table.Head>
             <Table.HeadCell>Nome</Table.HeadCell>
             <Table.HeadCell>Email</Table.HeadCell>
-            <Table.HeadCell>Tipo</Table.HeadCell>
+            <Table.HeadCell>Telefone</Table.HeadCell>
+            <Table.HeadCell>Especialidade</Table.HeadCell>
             <Table.HeadCell>Data de criação</Table.HeadCell>
             <Table.HeadCell>
               <span className="sr-only">Edit</span>
             </Table.HeadCell>
           </Table.Head>
           <Table.Body className="divide-y">
-            {mockedUsers.map(user => (
-              <Table.Row
-                key={user.id}
-                className="bg-white dark:border-gray-700 dark:bg-gray-800"
-              >
-                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                  {user.name}
-                </Table.Cell>
-                <Table.Cell>{user.email}</Table.Cell>
-                <Table.Cell>{user.type}</Table.Cell>
-                <Table.Cell>
-                  {user.created_at.toLocaleDateString('pt-BR')}
-                </Table.Cell>
-                <Table.Cell>
-                  <a className="font-medium text-cyan-700 hover:underline dark:text-cyan-600">
-                    <p>Edit</p>
-                  </a>
-                </Table.Cell>
-              </Table.Row>
-            ))}
+            {filteredData
+              .slice(
+                (currentPage - 1) * contentPerPage,
+                contentPerPage * currentPage,
+              )
+              .map(user => (
+                <Table.Row
+                  key={user.id}
+                  className="bg-white dark:border-gray-700 dark:bg-gray-800"
+                >
+                  <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                    {user.name}
+                  </Table.Cell>
+                  <Table.Cell>{user.email}</Table.Cell>
+                  <Table.Cell>{user.phone_number}</Table.Cell>
+                  <Table.Cell>{user.specialty}</Table.Cell>
+                  <Table.Cell>
+                    {new Date(user.created_at).toLocaleDateString('pt-BR')}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <a className="font-medium text-sky-500 hover:underline dark:text-sky-600 cursor-pointer">
+                      <p>Editar</p>
+                    </a>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
           </Table.Body>
         </Table>
       </div>
-    </>
+
+      <div className="flex justify-center sm:justify-end">
+        <Pagination
+          currentPage={currentPage}
+          onPageChange={page => setCurrentPage(page)}
+          totalQuantityOfData={filteredData.length}
+          dataPerPage={contentPerPage}
+        />
+      </div>
+    </div>
   );
 }
