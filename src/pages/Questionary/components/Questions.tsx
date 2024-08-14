eb387d69aedd19a07447c56c65e3284aaeedd6f7
Talles <talles17.a@hotmail.com>
2024-08-14 17:27:28 -0300

    fix: doc drawer error in questionary page

--- src/pages/Questionary/components/Questions.tsx
+++ src/pages/Questionary/components/Questions.tsx
@@ -85,7 +85,10 @@ export function Questions({ handleFinishForm }: QuestionsProps) {
           </span>
         </div>
         <div className="flex-shrink-0">
-          <button className="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
+          <button
+            type="button"
+            className="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+          >
             <HiMenu
               className="w-5 h-5"
               onClick={() => setDocsDrawerIsOpen(prevState => !prevState)}
