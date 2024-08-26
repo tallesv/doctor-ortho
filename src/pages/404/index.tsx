1dc91b0c73dcb33ab545a4ec22213d88e6e70b21
Talles <talles17.a@hotmail.com>
2024-08-26 17:12:31 -0300

    fix: 404 page error style

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -4,7 +4,7 @@ import { Button } from '../../components/Button';
 export function NotFoundPage() {
   const navigate = useNavigate();
   return (
-    <main className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+    <section className="grid min-h-full px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
         <p className="text-base font-semibold text-sky-600">404</p>
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-800 dark:text-white sm:text-5xl">
@@ -17,6 +17,6 @@ export function NotFoundPage() {
           <Button onClick={() => navigate(-1)}>Voltar</Button>
         </div>
       </div>
-    </main>
+    </section>
   );
 }
