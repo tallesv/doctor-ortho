5ec9e19b7acf48e7a6b51f0c3019c2d8161b10c4
Talles <talles17.a@hotmail.com>
2024-09-03 00:48:13 -0300

    chore: add 'import/prefer-default-export': 'off' rule in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,27 +17,19 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'react-hooks',
-    "prettier"
-  ],
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
   rules: {
+    'import/prefer-default-export': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/jsx-filename-extension': [
       1,
       {
-        extensions: [
-          '.tsx',
-        ],
+        extensions: ['.tsx'],
       },
     ],
     'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': [
-      'error',
-    ],
+    '@typescript-eslint/no-use-before-define': ['error'],
     'react/react-in-jsx-scope': 'off',
     'import/extensions': [
       'error',
@@ -47,7 +39,7 @@ module.exports = {
         tsx: 'never',
       },
     ],
-    "prettier/prettier": "error"
+    'prettier/prettier': 'error',
   },
   settings: {
     'import/resolver': {
