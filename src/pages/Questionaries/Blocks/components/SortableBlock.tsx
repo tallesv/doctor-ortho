815db53c317aad21e5cbbed286b9afbb0f643fbb
Talles <talles17.a@hotmail.com>
2024-09-25 23:57:29 -0300

    fix: draggable on mobile

--- src/pages/Questionaries/Blocks/components/SortableBlock.tsx
+++ src/pages/Questionaries/Blocks/components/SortableBlock.tsx
@@ -1,5 +1,5 @@
 import { Dropdown } from 'flowbite-react';
-import { HiDotsVertical } from 'react-icons/hi';
+import { HiDotsVertical, HiOutlineSwitchVertical } from 'react-icons/hi';
 import { Button } from '../../../../components/Button';
 import { BlockType } from '../../types';
 import { useSortable } from '@dnd-kit/sortable';
@@ -31,16 +31,21 @@ export function SortableBlock({
     <li
       ref={setNodeRef}
       {...attributes}
-      {...listeners}
       style={style}
-      className="flex justify-between gap-x-6 py-5"
+      className="flex justify-between gap-x-1 py-5 cursor-default"
     >
-      <div className="flex min-w-0 gap-x-4">
+      <div className="flex items-center min-w-0 gap-x-4">
+        <div
+          className="border border-gray-200 dark:border-gray-600 rounded p-1 cursor-pointer"
+          {...listeners}
+        >
+          <HiOutlineSwitchVertical className="w-5 h-5" />
+        </div>
         <div className="min-w-0 flex-auto">
           <p className="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-200">
             {block.name}
           </p>
-          <time className="mt-1 truncate text-xs leading-5 text-gray-500 dark:text-gray-400">
+          <time className="mt-1 text-xs leading-5 text-gray-500 dark:text-gray-400 flex-wrap">
             {`Atualizado em  ${new Date(
               block.updated_at,
             ).toLocaleDateString()} às ${new Date(
@@ -61,7 +66,7 @@ export function SortableBlock({
           renderTrigger={() => (
             <button
               type="button"
-              className="px-0 py-2.5 border-0 mr-2 mb-2 focus:ring-0 "
+              className="px-0 py-2.5 border-0 ml-2 focus:ring-0 "
               id="user-menu-button"
               aria-expanded="false"
             >
